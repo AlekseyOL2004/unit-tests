e9f93b814210ee454584ac6a74f8ece21a71de35
Олексій Олексюк <alekseyol2004@gmail.com>
2025-11-26 22:15:07 +0200

    Update Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,30 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import { defineConfig } from "eslint/config"; // ← саме так
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs"
+    }
+  },
+  {
+    files: ["**/*.test.js"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        test: "readonly",
+        expect: "readonly"
+      }
+    }
+  }
 ]);
